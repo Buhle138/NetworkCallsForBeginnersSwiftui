1cec1ef4173f7dbf3c544e562b4a1fd81edd7d57
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-17 12:22:53 +0200

    Initial Commit

---
+++ NetworkCallsForBeginnersSwiftui/NetworkCallsForBeginnersSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCallsForBeginnersSwiftuiApp.swift
+//  NetworkCallsForBeginnersSwiftui
+//
+//  Created by Buhle Radzilani on 2024/06/17.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCallsForBeginnersSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
